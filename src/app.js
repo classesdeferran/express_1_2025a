6c112835e1242c29df4177ee1f149570c701d24e
Ferran <ferran.docencia@gmail.com>
2025-06-16 12:06:00 +0200

    /public

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const path = require('node:path')
 const express = require('express')
 const server = express()
 const util = require('node:util')
@@ -5,8 +6,11 @@ const util = require('node:util')
 process.loadEnvFile()
 const PUERTO = process.env.PUERTO || 30000
 
+server.use(express.static(path.join(__dirname, "../public")))
+
 server.get("/", (req, res) => {
     console.log(util.styleText("green", "Conectados a HOME"));
+    console.log(util.styleText("blue", __dirname));
     res.send("<h1>Estamos en la ruta raiz o home</h1>")
 })
 
@@ -16,6 +20,10 @@ server.get("/clientes", (req, res) => {
 
 server.get("/api/cliente", (req, res) => {
     res.json({"nombre": "James", "apellido": "Bond", "direccion" : "Bahamas"})
+})
+
+server.get("/james", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/james.html"))
 })
 
 server.use( (req, res) => {
